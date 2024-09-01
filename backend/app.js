b659bc0c5c3d0ea9eac8b5d575a9ed7093aa7bfb
Aarush Kumar <2005aarushkumar@gmail.com>
2024-09-01 21:16:37 +0530

    Prepare for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(
     cors({
@@ -17,5 +20,12 @@ app.use(express.static("public"));
 import rantRoutes from "./routes/rant.routes.js";
 
 app.use("/api/v1/rant", rantRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (_, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 export { app };
